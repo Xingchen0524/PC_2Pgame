626fec9de1f2f9663ac3e01121c59498660e0990
ivy513 <144639153+ivy513@users.noreply.github.com>
2024-01-04 14:54:19 +0800

    首頁和設定頁面
    增加BGM

--- Assets/Menu.cs
+++ Assets/Menu.cs
@@ -1,9 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//使用UnityEngine;
+using UnityEngine.UI;
+
 
 public class Menu : MonoBehaviour
 {
+    [Header("首頁")]
+    public GameObject MenuPage;
+    [Header("設定")]
+    public GameObjeet SettingPage;
+    [Header("控制音量的Slider")]
+    public slider ControlVolumSlider
     // Start is called before the first frame update
     void Start()
     {
@@ -22,9 +31,19 @@ public class Menu : MonoBehaviour
     }
     public void Quit()
     {
-        //官必遊戲
+        //關閉遊戲
         Application.Quit();
     }
+
+public void SetSetting(bool Set)
+{
+    //控制首頁頁面關閉
+    MenuPage.SetActive(Set);
+    //控制設定頁面關閉
+    SettingPage.SetActive(!Set);
+}
+
+
 
         
         
