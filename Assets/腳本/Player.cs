6e0348bfdd3b69a212aa99d1baaf3e3e7e451ffa
Shineliu77 <shinecookie@gmail.com>
2024-02-05 20:14:39 +0800

    更新姊妹動畫
    更新姊妹動畫

--- Assets/腳本/Player.cs
+++ Assets/腳本/Player.cs
@@ -27,9 +27,14 @@ public class Player : MonoBehaviour
         //控制玩家位移
        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed,playerRB.velocity.y);
         //控制玩家移動時的動畫
-        anim.SetFloat("Speed",Mathf.Abs(playerRB.velocity.x));
+        //anim.SetFloat("Speed",Mathf.Abs(playerRB.velocity.x));
+        if(Input.GetAxis("Horizontal")!=0)
+            anim.SetBool("Walk", true);
+        else
+            anim.SetBool("Walk", false);
+
         //玩家移動翻轉x軸
-        if(playerRB.velocity.x > 0)
+        if (playerRB.velocity.x > 0)
         {
             theSR.flipX = true;
         }
