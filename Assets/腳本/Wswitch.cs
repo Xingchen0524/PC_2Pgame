a9444af91db915714f218a8c1914a338bbf331d2
Shineliu77 <33471613+Shineliu77@users.noreply.github.com>
2024-03-18 12:08:03 +0800

    修改切換腳色位置
    修改切換腳色位置

--- Assets/腳本/Wswitch.cs
+++ Assets/腳本/Wswitch.cs
@@ -1,24 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class NewBehaviourScript : MonoBehaviour
+using Cinemachine;
+public class Wswitch : MonoBehaviour
 {
     [Header("W切換場景")]
     public GameObject TipObj;
+    [Header("碰到牆壁的人")]
+    public GameObject User;
+    [Header("下一個座標位置")]
+    public Vector3 Pos;
+    [Header("設定CM vcam的Bounding Shape 2D")]
+    public CinemachineConfiner Obj;
+    [Header("下一個mapRange")]
+    public PolygonCollider2D mapRange;
     //角色碰到碰撞器會彈出W的提示框
     private void OnCollisionEnter2D(Collision2D Hit)
     {
-        if(Hit.collider.name == "Player")
+        if(Hit.collider.tag == "Player")
         {
+            User = Hit.collider.gameObject;
             TipObj.SetActive(true);
         }
     }
     //角色離開碰撞器提示框會收起來
     private void OnCollisionExit2D(Collision2D Hit)
     {
-        if (Hit.collider.name == "Player")
+        if (Hit.collider.tag == "Player")
         {
+            User = null;
             TipObj.SetActive(false);
         }
     }
@@ -29,10 +39,6 @@ public class NewBehaviourScript : MonoBehaviour
         
     }
 
-    [Header("女兒房間")]
-    public GameObject Daughterroom;
-    [Header("走廊")]
-    public GameObject Cooridor;
 
     public void ToGame()
     {
@@ -44,12 +50,10 @@ public class NewBehaviourScript : MonoBehaviour
     //當按下鍵盤上W會離開房間到走廊上
     void Update()
      {
-       if (Input.GetKeyDown(KeyCode.W))
+       if (Input.GetKeyDown(KeyCode.W)&& User!=null)
         {
-            //控制女兒房間頁面關閉
-            //(有問題的地方)Daughterroom.SetActive(Set);
-            //控制走廊頁面關閉
-            //(有問題的地方)Cooridor.SetActive(!Set);
+            User.transform.position = Pos;
+            Obj.m_BoundingShape2D = mapRange;
         }
     }   
 }
