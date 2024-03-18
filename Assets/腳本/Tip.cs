1a7fcbd47e3c4701ebd707322b841831c02eae81
ivy513 <144639153+ivy513@users.noreply.github.com>
2024-03-18 20:55:48 +0800

    場景切換成功
    場景切換成功

--- Assets/腳本/Tip.cs
+++ Assets/腳本/Tip.cs
@@ -2,14 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class tip : MonoBehaviour
+public class Tip : MonoBehaviour
 {
     [Header("提示")]
     public GameObject TipObj;
     //當玩家碰到客廳物品，就會有提示
     private void OnTriggerEnter2D(Collider2D Hit)
     {
-        if(Hit.name == "Player")
+        if(Hit.tag == "Player")
         {
             TipObj.SetActive(true);
         }
@@ -17,7 +17,7 @@ public class tip : MonoBehaviour
     //玩家離開，提示關閉
     private void OnTriggerExit2D(Collider2D Hit)
     {
-        if (Hit.name == "Player")
+        if (Hit.tag == "Player")
         {
             TipObj.SetActive(false);
         }
