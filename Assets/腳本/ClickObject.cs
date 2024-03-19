3b0a32f91811430fb9ce07060e93cf470700fd6f
ivy513 <144639153+ivy513@users.noreply.github.com>
2024-03-19 20:24:36 +0800

    0319中傳
    0319

---
+++ Assets/腳本/ClickObject.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickObject : MonoBehaviour
+{
+    //滑鼠點擊物件後，放大後消失
+    public class Example : MonoBehaviour
+    {
+        void OnMouseDown()
+        {
+            Debug.Log("滑鼠游標移到物件上");
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
