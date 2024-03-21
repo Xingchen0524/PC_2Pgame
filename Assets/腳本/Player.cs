a6b95e2f99e92ce6d845f657bc690ff21b8fa016
Shineliu77 <33471613+Shineliu77@users.noreply.github.com>
2024-03-21 16:39:04 +0800

    修改姐姐跟妹妹的人物操作
    修改姐姐跟妹妹的人物操作

--- Assets/腳本/Player.cs
+++ Assets/腳本/Player.cs
@@ -10,13 +10,16 @@ public class Player : MonoBehaviour
 
     [Header("玩家移動速度")]
     public float Speed;
+    float ScriptSpeed;
     public Rigidbody2D playerRB;
 
     private Animator anim;
     private SpriteRenderer theSR;
 
+    public string AxisHorizontal;
     void Start()
     {
+        ScriptSpeed = Speed;
         anim = GetComponent<Animator>();
         theSR = GetComponent<SpriteRenderer>();
     }
@@ -25,22 +28,28 @@ public class Player : MonoBehaviour
     void Update()
     {
         //控制玩家位移
-       playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * Speed,playerRB.velocity.y);
+       playerRB.velocity = new Vector2(Input.GetAxis(AxisHorizontal) * ScriptSpeed, playerRB.velocity.y);
         //控制玩家移動時的動畫
         //anim.SetFloat("Speed",Mathf.Abs(playerRB.velocity.x));
-        if(Input.GetAxis("Horizontal")!=0)
+        if (Input.GetAxis(AxisHorizontal) != 0)
+        {
+            ScriptSpeed = Speed;
             anim.SetBool("Walk", true);
+            //玩家移動翻轉x軸
+            if (playerRB.velocity.x > 0)
+            {
+                theSR.flipX = true;
+            }
+            else if (playerRB.velocity.x < 0)
+            {
+                theSR.flipX = false;
+            }
+        }
         else
+        {
+            ScriptSpeed = 0;
             anim.SetBool("Walk", false);
-
-        //玩家移動翻轉x軸
-        if (playerRB.velocity.x > 0)
-        {
-            theSR.flipX = true;
         }
-        else if(playerRB.velocity.x < 0)
-        {
-            theSR.flipX = false;
-        }
+    
     }
 }
