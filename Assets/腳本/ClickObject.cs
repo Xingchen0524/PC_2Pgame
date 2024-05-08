8892f0a108363643556a3a6200b61d97c738eaea
ivy513 <144639153+ivy513@users.noreply.github.com>
2024-05-08 13:27:56 +0800

    0508中途存檔
    0508中途存檔

--- Assets/腳本/ClickObject.cs
+++ Assets/腳本/ClickObject.cs
@@ -6,8 +6,28 @@ public class ClickObject : MonoBehaviour
 {
     [Header("被點擊物件")]
     public GameObject ClickObjcet;
-    [Header("精稿物件")]
+    [Header("物件")]
     public GameObject ObjcetAn;
+    [Header("提示")]
+    public GameObject TipObj;
+
+    //當玩家碰到客廳物品，就會有提示
+    private void OnTriggerEnter2D(Collider2D Hit)
+    {
+        if (Hit.tag == "Player")
+        {
+            TipObj.SetActive(true);
+        }
+    }
+    //玩家離開，提示關閉
+    private void OnTriggerExit2D(Collider2D Hit)
+    {
+        if (Hit.tag == "Player")
+        {
+            TipObj.SetActive(false);
+        }
+    }
+
 
     //點擊物件
     private void OnMouseDown()
@@ -15,17 +35,19 @@ public class ClickObject : MonoBehaviour
         ObjcetAn.SetActive(true);
         ClickObjcet.SetActive(false);
     }
-   
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetButtonUp("E"))
+        {
+            ObjcetAn.SetActive(true);
+            ClickObjcet.SetActive(false);
+        }
     }
+
+    
+    
+
+   
 }
 
