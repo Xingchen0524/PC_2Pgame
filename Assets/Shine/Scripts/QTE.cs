fc6bd62207bc6b790a0ece0f51a0fc3f4335c699
ivy513 <144639153+ivy513@users.noreply.github.com>
2024-05-10 18:18:33 +0800

    0510物件點擊修改
    0510物件點擊修改

---
+++ Assets/Shine/Scripts/QTE.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QTE : MonoBehaviour
+{
+    [Header("線的動畫")]
+    public Animator Line;
+    [Header("移動的線")]
+    public Transform MoveLine;
+    bool AnimatorState;
+
+    [Header("A正確值最大值")]
+    public float MaxNum_A;
+    [Header("A正確值最小值")]
+    public float MinNum_A;
+
+    [Header("B正確值最大值")]
+    public float MaxNum_B;
+    [Header("B正確值最小值")]
+    public float MinNum_B;
+
+    [Header("C正確值最大值")]
+    public float MaxNum_C;
+    [Header("C正確值最小值")]
+    public float MinNum_C;
+
+    
+    public float Test;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            AnimatorState = !AnimatorState;
+
+        }
+        if (AnimatorState)
+        {
+            Line.speed = 0;
+            Test = MoveLine.localPosition.x;
+            if (MoveLine.localPosition.x < MaxNum_A && MoveLine.localPosition.x >MinNum_A)
+            {
+                Debug.Log("A");
+            }
+            else if (MoveLine.localPosition.x < MaxNum_B && MoveLine.localPosition.x > MinNum_B)
+            {
+                Debug.Log("B");
+            }
+            else if (MoveLine.localPosition.x < MaxNum_C && MoveLine.localPosition.x > MinNum_C)
+            {
+                Debug.Log("C");
+            }
+            else
+            {
+                Debug.Log("失敗");
+            }
+        }
+        else
+        {
+            Line.speed = 1;
+        }
+    }
+}
--- Assets/腳本/ClickObject.cs
+++ Assets/腳本/ClickObject.cs
@@ -6,10 +6,15 @@ public class ClickObject : MonoBehaviour
 {
     [Header("被點擊物件")]
     public GameObject ClickObjcet;
-    [Header("物件")]
+    [Header("放大物件")]
     public GameObject ObjcetAn;
     [Header("提示")]
     public GameObject TipObj;
+    [Header("放大的背景")]
+    public GameObject Shady;
+    [Header("對話")]
+    public GameObject Dialogue;
+
 
     //當玩家碰到客廳物品，就會有提示
     private void OnTriggerEnter2D(Collider2D Hit)
@@ -35,9 +40,21 @@ public class ClickObject : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.E))
         {
+            //提示E關閉，被典籍物件關閉，放大物件打開，對話打開，黑幕打開
             TipObj.SetActive(false);
-            ObjcetAn.SetActive(true);
             ClickObjcet.SetActive(false);
+            ObjcetAn.SetActive(true);
+            Shady.SetActive(true);
+            Dialogue.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //提示E打開，被典籍物件打開，放大物件關閉，對話關閉，黑幕關閉
+            TipObj.SetActive(true);
+            ClickObjcet.SetActive(true);
+            ObjcetAn.SetActive(false);
+            Dialogue.SetActive(false);
+            Shady.SetActive(false);
         }
     }
 
